14be9fd3a039b63a9ab6a4550565711b2be787ce
Apple <apple@Apples-MacBook-Pro-5.local>
2019-07-26 08:04:47 -0700

    Iteration 2

--- emojinalproject/ViewController.swift
+++ emojinalproject/ViewController.swift
@@ -15,19 +15,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+     let flags = ["🇰🇭" : "Cambodia!", "🇯🇲" : "Jamaica!", "🇬🇱" : "Greenland!", "🇺🇬" : "Uganda!"]
+    
+    
     @IBAction func showMessage(sender: UIButton) {
         //the new screen needs to pop up
         // title should say : the flag is ....
         // can say did u guess correct
+        let selectedFlag = sender.titleLabel?.text
+   
         
-    let alertController = UIAlertController(title: "This flag is Cambodia" , message: "Did you guess correctly?" , preferredStyle: UIAlertController.Style.alert)
+        let alertController = UIAlertController(title: "This flag is \(flags[selectedFlag!]!)" , message: "Did you guess correctly?" , preferredStyle: UIAlertController.Style.alert)
         
         alertController.addAction(UIAlertAction(title:"OK", style: UIAlertAction.Style.default, handler: nil ))
     
         present(alertController, animated: true , completion: nil )
     }
     
-    
+
     
 }
 
